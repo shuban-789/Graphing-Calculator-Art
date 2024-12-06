9baccfb7259a357cc423c4336890e87603eae3ab
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2024-12-06 12:11:49 -0800

    Create one.m

---
+++ mod/one.m
@@ -0,0 +1,11 @@
+Plot[{mod(x^3,x)}, {x, -1, 1}]
+Plot[{mod(-x^3,x)}, {x, -1, 1}]
+
+Plot[{mod(x^3,-x)}, {x, -1, 1}]
+Plot[{mod(-x^3,-x)}, {x, -1, 1}]
+
+Plot[{mod(y^3,y)}, {y, -1, 1}]
+Plot[{mod(y^3,-y)}, {y, -1, 1}]
+
+Plot[{mod(-y^3,y)}, {y, -1, 1}]
+Plot[{mod(-y^3,-y)}, {y, -1, 1}]
